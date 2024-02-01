228da4faef71559e638a4eeb6dee567533609d54
ghast123424 <tomek.skorka22@gmail.com>
2024-02-01 15:11:35 +0100

    0.1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer/footer";
+import NavBar from "./components/nav/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ['400','700']  });
 
 export const metadata: Metadata = {
   title: "Tomek",
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <div className="flex flex-col min-h-screen" >
+        
+          <NavBar/>
+          <main className="flex-grow">{children}</main>
+          <Footer/>
+        </div>
+       
+      </body>
     </html>
   );
 }
